f537f962f30cee6f0503be7eb39a32eda8a2bdcd
Cameron <cameron.wragg@hotmail.co.uk>
2018-05-15 03:28:06 +0100

    Add files via upload

---
+++ FakeNewsCE/js/popup.js
@@ -0,0 +1,55 @@
+//var curntTab = window.location.href;
+var curntTab = "http://www.bbc.co.uk/news/world-middle-east-44116340";
+
+
+document.addEventListener('DOMContentLoaded', function()
+  {
+    var notifContainer = document.getElementById('choiceConfirmation');
+    var yesButton = document.getElementById('yesBtn');
+    var noButton = document.getElementById('noBtn');
+    var sendButton = document.getElementById('sendBtn')
+
+    sendButton.addEventListener('click', function()
+    {
+      var url = "ws://192.168.0.10:5000";
+      cnnws = new WebSocket(url);
+      cnnws.onopen = function()
+      {
+        cnnws.send(curntTab);
+        console.log('OPENED SOCKET AT' + url);
+        cnnws.onmessage() = function(x)
+        {
+          console.log(x.data);
+        }
+      }
+    });
+
+    yesButton.addEventListener('click', function()
+      {
+        var yesVal = document.getElementById('emailField').value;
+        if (yesVal == "") {
+          var data = {message: 'Please enter your email first.'};
+          notifContainer.MaterialSnackbar.showSnackbar(data);
+        } else {
+          var data = {message: 'You have trusted this article!'};
+          notifContainer.MaterialSnackbar.showSnackbar(data);
+          $('.queryCard > .mdl-card__title').css({'background-image': 'url(../images/cardBgHap.jpg)'});
+        }
+      });
+
+    noButton.addEventListener('click', function()
+      {
+        var noVal = document.getElementById('emailField').value;
+        if (noVal == "") {
+          var data = {message: 'Please enter your email first.'};
+          notifContainer.MaterialSnackbar.showSnackbar(data);
+        } else {
+          var data = {message: 'You do not trust this article!'};
+          notifContainer.MaterialSnackbar.showSnackbar(data);
+          $('.queryCard > .mdl-card__title').css({'background-image': 'url(../images/cardBgSad.jpg)'});
+        }
+      });
+
+  }, false);
+
+console.log(curntTab);
